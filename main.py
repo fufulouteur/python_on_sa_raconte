335d5297546ae010e44bf2fba91fbe8447ae7cde
fufulouteur <manzodqb@icloud.com>
2023-01-05 11:44:33 -0500

    fini project Python_on_sa_raconte

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ ami = input("le prenom d'un ami: ").capitalize()
 verbe = input("un verbe d'action: ").lower()
 adjectif3 = input("un autre adjectif: ").lower()
 
+
 histoire = (f"en ce jour d'ete sur la plage {adjectif1}, mes \
-ami et moi etions dans l'eau a jouer au  {jeu}.\
+ami et moi etions dans l'eau a jouer au  {jeu}.\n \
 soudain, une vague {adjectif2} arriva et mon pote {ami} a \
 crie, \"attention, une meduse {verbe}  !\" \
-{ami} a couru se refugier  sur la plage. \
+{ami} a couru se refugier  sur la plage. \n \
 Il avait tres peur des meduse qui {verbe}. les autre \
 sont reste a jouer au {jeu}. les meduses qui {verbe} sont \
-{adjectif3}.")
+{adjectif3}.\n ")
 
 print(histoire)
